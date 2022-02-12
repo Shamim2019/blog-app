b7fe2d6bef7ac8ea2be260900f260a5ac3c28364
Shamim <shamimmaloba@gmail.com>
2022-02-12 16:12:17 +0300

    added sqlalchemy

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class CodeSpeedyBlog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.Text, nullable=False)
+    posted_by = db.Column(db.String(20), nullable=False, default='N/A')
+    posted_on = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow())
+    def __repr__(self):
+        return self.title
+db.create_all()
+db.session.commit()
 
 @app.route('/')
 @app.route('/home')
 @app.route('/codespeedy')
+
 def welcome():
     return  render_template('index.html')
 
